5ece47e19c5cd49b7b01c2ae93eec79ec750bb47
Andrew Selzer <andrew.selzer@americanwell.com>
2019-04-16 15:34:33 -0400

    Adding open graph tags

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,10 @@
 module.exports = {
   description: 'Swing dance instructor, performer, and DJ based in Boston, Massachusetts.',
+  head: [
+    ['meta', { property: 'og:image', content: 'https://www.andrewselzer.com/hero.jpg' }],
+    ['meta', { property: 'og:title', content: 'Andrew Selzer' }],  
+    ['meta', { property: 'og:url', content: 'https://www.andrewselzer.com/' }],
+  ],
   title: 'Andrew Selzer',
   themeConfig: {
     sidebar: 'auto',
