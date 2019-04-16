8139d2c21da3154b40e10b6a957bd0f605861dad
Andrew Selzer <andrew.selzer@americanwell.com>
2019-04-16 11:14:15 -0400

    Changing headers to bold on about and adding sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,7 @@ module.exports = {
   title: 'Andrew Selzer',
   description: 'Swing dance instructor, organizer, and DJ based in Boston, Massachusetts.',
   themeConfig: {
+    sidebar: 'auto',
     nav: [
       { text: 'Home', link: '/' },
       { text: 'About', link: '/about.html' },
