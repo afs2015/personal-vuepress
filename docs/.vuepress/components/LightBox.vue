a81d5383139467f78ccd66ab0951880594af2961
Andrew Selzer <andrew.selzer@americanwell.com>
2019-04-17 15:02:46 -0400

    Inital pass at lightbox

---
+++ docs/.vuepress/components/LightBox.vue
@@ -0,0 +1,12 @@
+<template>
+  <a href="#" class="glightboxTest">
+    <img src="hero.jpg">
+  </a>
+</template>
+
+<script>
+  var lightboxDescription = GLightbox({
+    selector: 'glightboxTest'
+  });
+</script>
+      
