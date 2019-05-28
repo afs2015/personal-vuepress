88b85ee87c03a66457c54e00cee56e804d836edc
Andrew Selzer <andrew.selzer@americanwell.com>
2019-05-27 22:52:52 -0400

    Revert "Updating media gallery for reduced preview image size"
    This reverts commit f3442222f29490fbf458b6d88189d6e0f18ced39.

--- docs/.vuepress/components/MediaLayout.vue
+++ docs/.vuepress/components/MediaLayout.vue
@@ -1,54 +1,50 @@
 <template>
   <div class="container">
     <h1 class="title">Media Gallery</h1>
-    <div class="gallery-container">
-      <div class="responsive">
-        <div class="gallery">
-          <a target="_blank" href="Andrew_Andrea_Nevermore_2018.jpg">
-            <img src="Andrew_Andrea_Nevermore_2018.jpg" alt="Andrew Selzer and Andrea Byrnes competing at Nevermore Jazz Ball 2018" width="600" height="400">
-          </a>
-          <div class="desc">Photo by American Vernacular</div>
-        </div>
-      </div>
 
-      <div class="responsive">
-        <div class="gallery">
-          <a target="_blank" href="Cari_Andrew.jpg">
-            <img src="Cari_Andrew.jpg" alt="Andrew Selzer and Cari Meisel dancing in front of the Boston Custom House" width="600" height="400">
-          </a>
-          <div class="desc">Photo by Braden Nesin</div>
-        </div>
+    <div class="responsive">
+      <div class="gallery">
+        <a target="_blank" href="Andrew_Andrea_Nevermore_2018.jpg">
+          <img src="Andrew_Andrea_Nevermore_2018.jpg" alt="Andrew Selzer and Andrea Byrnes competing at Nevermore Jazz Ball 2018" width="600" height="400">
+        </a>
+        <div class="desc">Photo by American Vernacular</div>
       </div>
-
-      <div class="responsive">
-        <div class="gallery">
-          <a target="_blank" href="MariAndrewDip.jpg">
-            <img src="MariAndrewDip.jpg" alt="Andrew Selzer and Mari Adams dance at Jeanette Neil Dance Studio" width="600" height="400">
-          </a>
-          <div class="desc">Photo by Braden Nesin</div>
-        </div>
-      </div>
-
-      <div class="responsive">
-        <div class="gallery">
-          <a target="_blank" href="Katie_Andrew.jpg">
-            <img src="Katie_Andrew.jpg" alt="Katie Piselli and Andrew Selzer dancing in front of Boston Old State House" width="600" height="400">
-          </a>
-          <div class="desc">Photo by Braden Nesin</div>
-        </div>
-      </div>
-
-      <div class="clearfix"></div>
     </div>
+
+    <div class="responsive">
+      <div class="gallery">
+        <a target="_blank" href="Cari_Andrew.jpg">
+          <img src="Cari_Andrew.jpg" alt="Andrew Selzer and Cari Meisel dancing in front of the Boston Custom House" width="600" height="400">
+        </a>
+        <div class="desc">Photo by Braden Nesin</div>
+      </div>
+    </div>
+
+    <div class="responsive">
+      <div class="gallery">
+        <a target="_blank" href="MariAndrewDip.jpg">
+          <img src="MariAndrewDip.jpg" alt="Andrew Selzer and Mari Adams dance at Jeanette Neil Dance Studio" width="600" height="400">
+        </a>
+        <div class="desc">Photo by Braden Nesin</div>
+      </div>
+    </div>
+
+    <div class="responsive">
+      <div class="gallery">
+        <a target="_blank" href="Katie_Andrew.jpg">
+          <img src="Katie_Andrew.jpg" alt="Katie Piselli and Andrew Selzer dancing in front of Boston Old State House" width="600" height="400">
+        </a>
+        <div class="desc">Photo by Braden Nesin</div>
+      </div>
+    </div>
+
+    <div class="clearfix"></div>
   </div>
 </template>
 
 <style>
 
-  .gallery-container {
-    align-items: center;
-    display: flex;
-    justify-content: center;
+  .container {
     padding: 0 30px;
   }
 
@@ -78,7 +74,7 @@
   .responsive {
     padding: 0 6px;
     float: left;
-    width: 20%;
+    width: 24.999%;
   }
 
   @media only screen and (max-width: 700px) {
